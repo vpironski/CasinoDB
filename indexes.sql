344aece9c62896e2e838412dd2770052b8a79829
Deyan Malchev <116379803+Deiangh@users.noreply.github.com>
2024-05-28 16:37:52 +0300

    Update indexes.sql

--- indexes.sql
+++ indexes.sql
@@ -2,4 +2,4 @@ CREATE INDEX idx_email ON users (email);
 
 CREATE INDEX idx_username_email ON users (username, email);
 
-CREATE INDEX idx_account_number_prefix ON bank_accounts (account_number(10));
+CREATE INDEX idx_traffic_prefix ON most_played (traffic(5));
