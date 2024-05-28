1d46d9f64fb3adb7be72e1ddcbdec628c984b41d
Deyan Malchev <116379803+Deiangh@users.noreply.github.com>
2024-05-28 16:56:54 +0300

    Update indexes.sql

--- indexes.sql
+++ indexes.sql
@@ -4,4 +4,5 @@ CREATE INDEX idx_email ON users (email);
 
 CREATE INDEX idx_email_password ON users (email, password);
 
-CREATE INDEX idx_traffic_prefix ON most_played (traffic(5));
+-- Statistics --
+CREATE INDEX idx_reason_to_be_banned ON banlist (reason(30));
